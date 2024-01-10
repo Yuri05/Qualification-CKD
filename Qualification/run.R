09206ddf101329646638d576e3eab373e588d755
Yuri05 <Yuri05@github.com>
2024-01-10 13:33:50 +0100

    RE 2.2.315

--- Qualification/run.R
+++
@@ -1,8 +0,0 @@
-rm(list = ls())
-source("C:/dev/Qualification-CKD/Qualification/workflow.R")
-
-createQualificationReport(
-   qualificationRunnerFolder="C:/dev/QualificationRunner/src/QualificationRunner/bin/Debug/net472",
-   pkSimPortableFolder= "C:/dev/Releases/v11/PK-Sim_11.0.150",
-   versionInfo = QualificationVersionInfo$new("1.0", "11.0", "3.0")
-   )
